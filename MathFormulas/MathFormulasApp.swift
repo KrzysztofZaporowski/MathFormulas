2920c19f0d9da3ee2a616f5894ba960ca0cdcf12
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2026-02-23 19:06:19 +0100

    Initial Commit

---
+++ MathFormulas/MathFormulasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathFormulasApp.swift
+//  MathFormulas
+//
+//  Created by Krzysztof Zaporowski on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MathFormulasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
